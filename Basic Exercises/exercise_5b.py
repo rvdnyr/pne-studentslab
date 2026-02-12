5550037b6b9596b1d6dc69765d753f2f2af44c97
rvdnyr <gjmaterivad@gmail.com>
2026-02-12 13:08:52 +0100

    while loops

---
+++ Basic Exercises/exercise_5b.py
@@ -0,0 +1,11 @@
+# EXERCISE 5: LOOPS
+# 5b: WHILE loops
+
+# Write a program that starts with a variable n = 1 and keeps doubling it
+# (n = n * 2) until n is 1024 (included). Print each value of n.
+
+
+n = 1
+while n < 1024:
+    n = n * 2
+    print(n)
